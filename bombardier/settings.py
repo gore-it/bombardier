aebc9b469f74bf55871ee8f8c7877734b3c327b0
Pawel Szczepkowski <pawel.szczepkowski@fingo.info>
2014-05-27 11:36:45 +0200

    admin css

--- bombardier/settings.py
+++ bombardier/settings.py
@@ -83,6 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 print os.path.join(BASE_DIR, "static")
 
