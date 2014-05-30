4e9a52a3ed1ba65ad69e96cc33eb53aa6072b388
g0re1 <p.szczepkowski87@gmail.com>
2014-05-30 22:30:04 +0200

    changes .. settings

--- bombardier/settings.py
+++ bombardier/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '@he)!@p7r@upzi-m28vee9(%sf%z(e!+0$no2(v-rwa9&jk=xw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
